fb76dcfa2dac42373243da95c7ecd704c5185066
Martin Lecerf <lecerf.mar@orange.fr>
2021-01-18 12:17:04 +0100

    exo-4.1 nom monstre

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 import jdr.monde.Monde;
+import jdr.personnage.Monstres;
 import jdr.personnage.Personnage;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Personnage joueur = Monde.personnageFactory();
-		Monde.afficherInformations(joueur);
+		//Personnage joueur = Monde.personnageFactory();
+		Monstres monstre = Monde.MonstreFactory();
+		Monde.afficherInformations(null, monstre);
 	}
 	
 	
--- src/jdr/monde/monde.java
+++ src/jdr/monde/monde.java
@@ -1,36 +1,99 @@
 package jdr.monde;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
+import jdr.personnage.Monstres;
 import jdr.personnage.Personnage;
 
 public class Monde {
 		
 	/**
-	 * 
+	 * Creation d'un personnage
 	 * @return
 	 */
 	public static Personnage personnageFactory() {
 			
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez choisir un nom");
+		
+		System.out.println("Veuillez choisir un nom : \n");
 		String nom = sc.nextLine();
-		System.out.println("Veuillez entrer vos point de vie");
+		
+		System.out.println("Veuillez entrer vos point de vie : \n");
 		int pointDeVie = sc.nextInt();
-		System.out.println("Veuillez entrer vos point de degat");
+		
+		System.out.println("Veuillez entrer vos point de degat : \n");
 		int degat = sc.nextInt();
-		Personnage joueur = new Personnage(nom, degat, degat) ; 
+		
+		Personnage joueur = new Personnage(nom, pointDeVie, degat) ; 
+		
 		sc.close();
+		
 		return joueur;
 		
+	}
+	
+	
+	
+	/**
+	 * Creation d'un monstre
+	 * @return
+	 */
+	public static Monstres MonstreFactory()
+	{
+		
+		Scanner sc = new Scanner(System.in);
+
+		String nom = nomMonstre();
+		
+		System.out.println("Veuillez entrer vos point de vie : \n");
+		int pointDeVie = sc.nextInt();
+		
+		System.out.println("Veuillez entrer vos point de degat : \n");
+		int degat = sc.nextInt();
+		
+		Monstres monstre = new Monstres(nom, pointDeVie, degat) ; 
+		
+		sc.close();
+		
+		return monstre;
 	}
 	
 	/**
 	 * 
 	 * @param joueur
 	 */
-	public static void afficherInformations(Personnage joueur) {
+	public static void afficherInformations(Personnage joueur, Monstres monstre) {
 		System.out.println(joueur);
+		System.out.println(monstre);
+	}
+	
+	/**
+	 * Genereation du nom du monstre
+	 * @return nom
+	 */
+	public static String nomMonstre() {
+		List<String> debutNom = new ArrayList<String>();
+		debutNom.add("chat");
+		debutNom.add("chien");
+		debutNom.add("chaton");
+		
+		List<String> finNom = new ArrayList<String>();
+		finNom.add("mechant");
+		finNom.add("de feu");
+		finNom.add("de la mort");
+		
+		int MaxDebutNom = debutNom.size();
+		int nombreAleatoireListDebutNom = 0 + (int)(Math.random() * ((MaxDebutNom - 0) + 1));
+		
+		int MaxFinNom = finNom.size();
+		int nombreAleatoireListFinNom = 0 + (int)(Math.random() * ((MaxFinNom - 0) + 1));
+		
+		String nom = debutNom.get(nombreAleatoireListDebutNom) +" "+ finNom.get(nombreAleatoireListFinNom);
+		
+		return nom;
 	}
 
 }
